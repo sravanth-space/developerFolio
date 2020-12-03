1f3d2bba1c8ef2e0f32fd4b64c56d397b81d15ca
sravanthbaratam <sravanthbaratam@gmail.com>
2020-12-03 17:39:52 +0000

    Deploying to gh-pages from  @ 1be95adb5bdf149cef8110a5c7b32f4ce846539a 🚀

---
+++ precache-manifest.4a7a5018d2ca932fe01a0276e0da03d7.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f6c2e173fb70c6fb61b022d146c9da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a6375f529b2cd2ecc95",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "7b1842fd7141e35079f1",
+    "url": "/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "609eb88b0e5b93be5d12",
+    "url": "/static/css/main.8fe75b39.chunk.css"
+  },
+  {
+    "revision": "b72f114c8c394a98aa9d",
+    "url": "/static/js/2.30256bb7.chunk.js"
+  },
+  {
+    "revision": "8a6375f529b2cd2ecc95",
+    "url": "/static/js/3.6b1d3095.chunk.js"
+  },
+  {
+    "revision": "7b1842fd7141e35079f1",
+    "url": "/static/js/4.173e2dcd.chunk.js"
+  },
+  {
+    "revision": "609eb88b0e5b93be5d12",
+    "url": "/static/js/main.ade18435.chunk.js"
+  },
+  {
+    "revision": "bf026208121e657b7589",
+    "url": "/static/js/runtime-main.2fcb2fb8.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "c1b038f02d19816aea5cdd6299648c13",
+    "url": "/static/media/download.c1b038f0.jfif"
+  },
+  {
+    "revision": "c4dec27034b1e60eb9f8719ead40ecc9",
+    "url": "/static/media/download1.c4dec270.jfif"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "83a05b10639d238bd8c5bbab279ab911",
+    "url": "/static/media/harvardLogo.83a05b10.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "df8769e8b290156da12f24c4276d1253",
+    "url": "/static/media/wipro.df8769e8.jfif"
+  }
+]);
