befb3b59298141b8d6b8cd1798c86f70c563d3c0
sravanthbaratam <sravanthbaratam@gmail.com>
2023-01-13 19:32:16 +0530

    Added sentry.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from "react";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import { createRoot } from 'react-dom/client'
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 const container = document.getElementById('root')
 
