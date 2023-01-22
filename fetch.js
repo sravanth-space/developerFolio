ea8b925317fdcbc5ea00f7d713ed799746ddd115
sravanthbaratam <sravanthbaratam@gmail.com>
2023-01-22 11:24:33 +0530

    chore(scrape): Added repo variable.

--- fetch.js
+++ fetch.js
@@ -5,7 +5,7 @@ process = require("process");
 require("dotenv").config();
 
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
+const GITHUB_USERNAME = process.env.USERNAME_GITHUB;
 const USE_GITHUB_DATA = process.env.USE_GITHUB_DATA;
 const MEDIUM_USERNAME = process.env.MEDIUM_USERNAME;
 
